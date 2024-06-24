b6698e13bf3b05e35cb8c278aebe2d16f2f16faa
kaylee  ᕕ(ᐛ)ᕗ <133055878+kaybarr4@users.noreply.github.com>
2024-06-24 09:47:51 -0400

    Update GPR CV.py

--- GPR CV.py
+++ GPR CV.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
 
 ##### KEPLERIAN SIGNAL HELPER FUNCTIONS #####
@@ -138,7 +137,7 @@ def build_covariance_matrix(p1, p2, kernel_function):
         kernel_function (func): describes the covariance between two points
 
     Return:
-        cov_matrix (np.array): an array of size NxM filled with values computed by kernel
+        cov_matrix (np.array): array of size NxM filled with values computed by kernel
     '''
     cov_matrix = np.zeros((len(p1),len(p2)))
 
@@ -157,8 +156,7 @@ class CrossValidation():
         rv_measurements (np.array): array of measured radial velocity values
         rv_errors (np.array): array of radial velocity errors
         kernel_function (func): describes the covariance between two points
-        orbit_params (array or list): [transit time, period, eccentricity, omega, and keplerian semi-amplitude] 
-        train_split (float): number from 0 to 1 that will divide data into a training and testing set
+        orbit_params (array): [transit time, period, eccentricity, omega, and Keplerian semi-amplitude] 
         gamma (float, optional): Defaults to 0.
         jitter (float, optional): Defaults to 0.
 
@@ -174,6 +172,9 @@ class CrossValidation():
 
 
     def Split(self, train_split):
+        """
+        train_split (float): number from 0 to 1 that will divide data into a training and testing set
+        """
         ######## selecting training & test set at random ########
         n_data = len(self.times)
         training_mask = np.random.choice(n_data, size=int(train_split * n_data), replace=False)
@@ -183,6 +184,10 @@ class CrossValidation():
         self.test_mask = test_mask
 
     def GP_Predict(self, times_predict, include_keplerian = True):
+        """
+        times_predict (np.array): set of times for the GPR to predict values for
+        include_keplerian (bool): whether or not to subtract planetary signal in predictions. Defaults to True.
+        """
 
         if include_keplerian:
             mean_function = calc_keplerian_signal(self.times, *self.orbit_params) + self.gamma
@@ -230,6 +235,10 @@ class CrossValidation():
         return CV_means, CV_variances
 
     def Plot(self, times_predict, include_keplerian = True):
+        """
+        times_predict (np.array): set of times for the GPR to predict values for
+        include_keplerian (bool): whether or not to subtract planetary signal in predictions. Defaults to True.
+        """
 
         predictive_means, predictive_variance = self.GP_Predict(times_predict, include_keplerian)
         std_dev = np.sqrt(predictive_variance)
@@ -297,6 +306,8 @@ class CrossValidation():
         plt.close()
 
 
+
+###### building test kernel #####
 def quasi_periodic_kernel(ti, tj):
     '''
     Computes values for quasi-periodic covariance function.
@@ -309,179 +320,6 @@ def quasi_periodic_kernel(ti, tj):
     '''
     return GP_amplitude**2 * np.exp(-((np.sin(np.pi * (ti-tj) / P_gp_test ))**2)/(2*lambda_p_test**2) - ((ti-tj)**2 / (lambda_e_test**2)))
 
-
-
-###### dai et al 2017 plots ######
-
-# ### importing test data ###
-df = pd.read_csv('dai et al 2017 pfs.csv')
-
-data_times_pfs = df['BJD_TDB'].to_numpy()
-data_rv_pfs = df['RV (m/s)'].to_numpy()
-data_rv_errors_pfs = df['Unc (m/s)'].to_numpy()
-
-
-df = pd.read_csv('dai et al 2017 harps.csv')
-
-data_times_harps = df['BJD_TDB'].to_numpy()
-data_rv_harps = df['RV (m/s)'].to_numpy()
-data_rv_errors_harps = df['Unc (m/s)'].to_numpy()
-
-data_period = 0.3693038
-data_transit_time = 2457582.9360
-data_ecc = 0
-data_kepler_semiamp = 6.77
-data_omega = 0
-
-GP_amplitude = 26
-P_gp_test = 9.68
-lambda_e_test = 11.6
-lambda_p_test = 0.35
-
-data_orbit_params = [data_transit_time, data_period, data_ecc, data_omega, data_kepler_semiamp]
-t_predict_harps = np.linspace(np.min(data_times_harps), np.max(data_times_harps), int(1e3))
-t_predict_pfs = np.linspace(np.min(data_times_pfs), np.max(data_times_pfs), int(1e3))
-
-
-cv_harps = CrossValidation(data_times_harps, data_rv_harps, data_rv_errors_harps, quasi_periodic_kernel, data_orbit_params, -6694.7, 2)
-cv_pfs = CrossValidation(data_times_pfs, data_rv_pfs, data_rv_errors_pfs, quasi_periodic_kernel, data_orbit_params, -1, 5.3)
-
-cv_harps.Split(0.8)
-cv_pfs.Split(1)
-
-cv_harps.Plot(t_predict_harps)
-
-# harps_predict_means, harps_predict_var = cv_harps.GP_Predict(t_predict_harps)
-# pfs_predict_means, pfs_predict_var = cv_pfs.GP_Predict(t_predict_pfs)
-
-# harps_fit, _ = cv_harps.GP_Predict(data_times_harps)
-
-# pfs_fit, _ = cv_pfs.GP_Predict(data_times_pfs)
-
-# fig, (ax1, ax2) = plt.subplots(2, sharex=True, height_ratios = [0.6, 0.4])
-# fig.subplots_adjust(hspace=0)
-
-# ax1.plot(t_predict_harps, harps_predict_means, color = 'b')
-# ax1.plot(t_predict_pfs, pfs_predict_means, color = 'b')
-
-
-# ax1.fill_between(t_predict_harps, harps_predict_means - np.sqrt(harps_predict_var), harps_predict_means + np.sqrt(harps_predict_var), color = 'r', alpha = 0.1)
-# ax1.fill_between(t_predict_harps, harps_predict_means- 2*np.sqrt(harps_predict_var) , harps_predict_means + 2*np.sqrt(harps_predict_var) , color = 'r', alpha = 0.2)
-
-# ax1.fill_between(t_predict_pfs, pfs_predict_means- np.sqrt(pfs_predict_var),  pfs_predict_means + np.sqrt(pfs_predict_var), color = 'tab:pink', alpha = 0.1)
-# ax1.fill_between(t_predict_pfs, pfs_predict_means - 2*np.sqrt(pfs_predict_var),  pfs_predict_means + 2*np.sqrt(pfs_predict_var), color = 'tab:pink', alpha = 0.2)
-
-
-# ax1.errorbar(data_times_harps, data_rv_harps + 6694.7, data_rv_errors_harps, ls="", marker="^", color="r", ecolor = '0.5', label="HARPS-N")
-# ax1.errorbar(data_times_pfs, data_rv_pfs, data_rv_errors_pfs, ls="", marker="o", color="tab:pink", ecolor = '0.5',  label="PFS")
-
-# ax1.legend(fontsize = 8)
-
-# ax2.axhline(color="tab:grey", ls="--")
-
-# ax2.errorbar(data_times_harps, data_rv_harps + 6694.7 - harps_fit, data_rv_errors_harps, ls="", marker="^", color="r", ecolor = '0.5', label="HARPS-N")
-# ax2.errorbar(data_times_pfs, data_rv_pfs - pfs_fit, data_rv_errors_pfs, ls="", marker="o", color="tab:pink", ecolor = '0.5' , label="PFS")
-
-
-
-# ax2.set_xlabel("Time")
-# ax2.set_ylabel("RV residual")
-# ax1.set_ylabel("RV")
-
-
-
-# plt.show()
-
-
-
-
-
-# ##### barragan et al 2024 plots ######
-
-# ### importing test data ###
-# df = pd.read_csv('harpstable.csv')
-
-# data_times = df['#Time (BJD - 2457000)'].to_numpy()
-# data_times += 2457000 ### revert to BJD
-# data_rv_gaussian = df[' RV Gaussian [km/s]'].to_numpy()
-# data_rv_GND = df['RV GND [km/s]'].to_numpy()
-# data_rv_GND_28 = df[' RV GND_2.8 [km/s] '].to_numpy()
-# data_rv_errors = df['eRV[km/s] '].to_numpy()
-
-
-# data_period = 8.3249113
-# data_transit_time = 2356.81398 + 2457000
-# data_ecc = 0
-# data_kepler_semiamp = 0.0346
-# data_omega = 0
-
-# GP_amplitude = 0.138
-# P_gp_test = 3
-# lambda_p_test = 0.42
-# lambda_e_test = 69
-
-
-# data_orbit_params = [data_transit_time, data_period, data_ecc, data_omega, data_kepler_semiamp]
-# t_predict = np.linspace(np.min(data_times), np.max(data_times), int(1e3))
-
-
-
-
-# my_cv = CrossValidation(data_times, data_rv_GND_28, data_rv_errors, quasi_periodic_kernel, data_orbit_params, -0.002, 0.026)
-# my_cv.Split(0.7)
-# my_cv.Plot(t_predict)
-
-
-# stellar_signal_plus_kepler_means, stellar_signal_plus_kepler_variances = my_cv.GP_Predict(t_predict, False)
-
-
-# stellar_signal_only_means, stellar_signal_only_variances =  my_cv.GP_Predict(t_predict, True)
-# stellar_signal, _ = my_cv.Run_CV()
-
-
-
-
-
-
-
-# keplerians = calc_keplerian_signal(t_predict, *data_orbit_params)
-# keplerians2 = calc_keplerian_signal(data_times, *data_orbit_params)
-
-
-# fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, height_ratios = [0.55, 0.25, 0.20])
-# fig.subplots_adjust(hspace=0)
-
-# ax1.plot(t_predict, stellar_signal_plus_kepler_means, label = "Stellar Signal + Keplerians", color = 'k')
-# ax1.plot(t_predict, stellar_signal_only_means - 0.5, label = "Stellar Signal", color = 'b')
-# ax1.plot(t_predict, keplerians - 1, label = "Keplerians", color = 'g')
-
-# ax1.fill_between(t_predict, stellar_signal_plus_kepler_means- np.sqrt(stellar_signal_plus_kepler_variances), stellar_signal_plus_kepler_means + np.sqrt(stellar_signal_plus_kepler_variances), color = '#808080', alpha = 0.3)
-# ax1.fill_between(t_predict, stellar_signal_plus_kepler_means- 2*np.sqrt(stellar_signal_plus_kepler_variances), stellar_signal_plus_kepler_means + 2*np.sqrt(stellar_signal_plus_kepler_variances), color = '#A9A9A9', alpha = 0.5)
-
-# ax1.fill_between(t_predict, stellar_signal_only_means- np.sqrt(stellar_signal_only_variances) - 0.5, stellar_signal_only_means + np.sqrt(stellar_signal_only_variances) - 0.5, color = 'b', alpha = 0.1)
-# ax1.fill_between(t_predict, stellar_signal_only_means- 2*np.sqrt(stellar_signal_only_variances) - 0.5, stellar_signal_only_means + 2*np.sqrt(stellar_signal_only_variances) - 0.5, color = 'b', alpha = 0.2)
-
-
-# ax1.errorbar(data_times, data_rv_GND_28, data_rv_errors, ls="", marker="o", color="r", label="RV")
-
-# ax1.legend(fontsize = 8)
-
-# ax3.axhline(color="g", ls="--")
-
-
-# ax2.plot(t_predict, keplerians,label = "Keplerians", color = 'g')
-# ax2.errorbar(data_times, data_rv_GND_28 - stellar_signal, data_rv_errors, ls="", marker="o", color="r", label="RV - Stellar Signal")
-
-# ax3.errorbar(data_times, data_rv_GND_28 - stellar_signal - keplerians2, data_rv_errors, ls="", marker="o", color="r")
-# ax2.legend(fontsize = 8)
-
-
-# ax3.set_xlabel("Time")
-# ax3.set_ylabel("RV residual")
-# ax1.set_ylabel("RV")
-# ax2.set_ylabel("RV")
-
-# plt.show()
 
 
 
